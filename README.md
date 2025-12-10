4e1c890faefd5e132875d908fe82f4264f7b20fa
Alexandre Pereira <alexpln259@gmail.com>
2025-12-09 22:15:27 -0300

    Estudo de react

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Estudo-de-react
+# Estudo de React
+
+Projeto de estudos utilizando React com Vite.
+
+## Tecnologias
+
+- React
+- Vite
+- JavaScript
+- ESLint
+
+## Plugins oficiais
+
+- @vitejs/plugin-react (Babel + Fast Refresh)
+- @vitejs/plugin-react-swc (SWC + Fast Refresh)
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w[500px] bg-slate-600 rounded-lg p-6">
+        <h1 className="text-slate-100 font-bold text-center text-3xl">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
+    </div>
+  );
+}
+
+export default App;

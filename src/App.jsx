42ae65c83cdbdbe74b96c7b724d3d6fc8c8a8f59
Alexandre Pereira <alexpln259@gmail.com>
2025-12-11 06:56:27 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w[500px] bg-slate-600 rounded-lg p-6">
         <h1 className="text-slate-100 font-bold text-center text-3xl">
-          Gerenciador de Tarefas
+          Gerenciador de Tarefas - inicial
         </h1>
         <AddTask />
         <Tasks />

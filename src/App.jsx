b2a9257132fd0655c6cd25524a7526c67e6441ce
Alexandre Pereira <alexpln259@gmail.com>
2025-12-11 07:05:36 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AddTask from "./components/AddTask";
 function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w[500px] bg-slate-600 rounded-lg p-6">
+      <div className="w[500px] bg-slate-700 rounded-lg p-6">
         <h1 className="text-slate-100 font-bold text-center text-3xl">
           Gerenciador de Tarefas - inicial
         </h1>

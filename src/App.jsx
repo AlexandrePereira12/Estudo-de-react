4662ac439100b7cf4e0052789bc1755ba89d88bd
Alexandre Pereira <alexpln259@gmail.com>
2025-12-15 21:27:25 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w[500px] bg-slate-700 rounded-lg p-6">
         <h1 className="text-slate-100 font-bold text-center text-3xl">
-          Gerenciador de Tarefas - inicial
+          Gerenciador de Tarefas - inicial pengas
         </h1>
         <AddTask />
         <Tasks />

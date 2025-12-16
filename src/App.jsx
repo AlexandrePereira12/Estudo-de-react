525fc97e7094081a66df96ef5ce79505ba57d862
Alexandre Pereira <alexpln259@gmail.com>
2025-12-15 21:46:58 -0300

    Avancando nos estudos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Pengas",
+      isCompleted: false,
+    },
+    { id: 2, title: "Fazer compras", description: "Pengas", isCompleted: true },
+    { id: 3, title: "Ler um livro", description: "Pengas", isCompleted: false },
+  ]);
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w[500px] bg-slate-700 rounded-lg p-6">
@@ -9,7 +21,7 @@ function App() {
           Gerenciador de Tarefas - inicial pengas
         </h1>
         <AddTask />
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,6 @@
-function Tasks() {
-  return (
-    <div className="App">
-      <h1>Tasks</h1>
-    </div>
-  );
+function Tasks(props) {
+  console.log(props);
+  return <h1>{props.tasks[0].title}</h1>;
 }
 
 export default Tasks;
